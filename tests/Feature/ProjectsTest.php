689841136de98d2287737364ec98bd5f1108357a
Arif Khan <arifkhan.incaendo@gmail.com>
2019-01-03 07:03:29 +0530

    lesson 8

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -12,11 +12,26 @@ class ProjectsTest extends TestCase
 
 
     /** @test */
-    public function only_authenticated_user_can_create_porjects()
+    public function guests_can_not_create_porjects()
     {
         $attributes = factory('App\Project')->raw();
 
         $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guest_may_not_view_projects()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+
+    /** @test */
+    public function guest_cannot_view_a_single_projects()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertRedirect('login');
     }
 
     /** @test */
@@ -34,15 +49,26 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_user_can_view_a_project()
     {
+        $this->be(factory('App\User')->create());
+
         $this->withoutExceptionHandling();
 
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
     }
 
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->be(factory('App\User')->create());
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
+    }
 
     /** @test */
     public function a_project_requires_a_title()
